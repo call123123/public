86dc6edc096b6b11e583d83f35d96dd333301d16
1948824280@qq.com <1948824280@qq.com>
2023-11-10 11:54:08 +0800

    init

---
+++ hetu-config/src/main/java/com/cjs/Test.java
@@ -0,0 +1,13 @@
+package com.cjs;
+
+import com.hetu.minimalconfig.utils.ConfigAesUtils;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ConfigAesUtils.encrypt("");
+
+	}
+
+}
